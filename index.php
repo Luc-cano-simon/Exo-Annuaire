c0edcf08d6e6937432205d5c5efb117b7049fa6a
luc <luc.cano@hotmail.fr>
2018-06-10 18:22:10 +0200

    suite

---
+++ index.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="style.css" />
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+	<title> Annuaire </title>
+
+</head>
+<body>
+												<!-- cmd pour inclure php dans html -->
+<?php
+include("Annuaire.php");
+?>
+
+	<h1> Le simpl'Annuaire </h1>
+
+												<!-- Menu horizontal -->
+
+<ul>
+        <li><a href="index2.php" style="color: black"> Demande d'inscription </a></li> 
+        
+        <li><a href="https://www.pagesjaunes.fr/pagesblanches" title="Numéro hors simplon c'est ici" style="color: black">Recherche</a></li>
+    </ul>
+
+
+
+    											<!-- Tableau titre -->
+
+      <table class="highlight">
+        
+          <tr>
+              <th width="20%" align="center"> Nom et Prénom </th>
+              <th width="20%" align="center"> Code postal </th>
+              <th width="20%" align="center"> Ville </th>
+              <th width="20%" align="center"> Numéro de téléphone </th>
+              <th width="20%" align="center"> Email </th>
+          </tr>
+        										<!-- Tableau contenu -->
+<tbody>
+        <tr>
+            <td> Océane	Gatien </td>
+            <td> 31430 </td>
+            <td> Vinça / Polastron  </td>
+            <td> 06 42 48 98 08 </td>
+			<td> ogatien@simplon.co </td>
+        </tr>
+        <tr>
+            <td> Aurélien	Chirot </td>
+            <td> 32000  </td>
+            <td> Auch </td>
+            <td> 06 77 13 21 22 </td>
+			<td> achirot@simplon.co </td>
+        </tr>
+        <tr>
+            <td> Philippe Jonathan </td>
+            <td> 32270  </td>
+            <td> Aubiet </td>
+            <td> 06 12 39 50 35 </td>
+			<td> philippe1jonathan@gmail.com </td>
+        </tr>
+        <tr>
+            <td> Cano-simon Luc </td>
+            <td> 32500  </td>
+            <td> Fleurance </td>
+            <td> 06 89 02 63 87 </td>
+			<td> luc.cano@hotmail.fr </td>
+        </tr>
+        <tr>
+            <td> Badji Adeline </td>
+            <td> 32000  </td>
+            <td> Auch </td>
+            <td> 06 84 62 33 30 </td>
+			<td> adeline.contact@gmail.com </td>
+        </tr>
+        <tr>
+            <td> Marc Lupi	</td>
+            <td> 32000  </td>
+            <td> Auch </td>
+            <td> 07 82 10 80 37 </td>
+			<td> marc.lupi@protonmail.com </td>
+        </tr>
+        <tr>
+            <td> Alexandra Kurowski</td>
+            <td> 32000  </td>
+            <td> Auch </td>
+            <td> 06 61 52 19 76 </td>
+			<td> alexandrakuro@gmail.com </td>
+        </tr>
+        <tr>
+            <td> Marco Oliveira Ribeiro</td>
+            <td> 32000  </td>
+            <td> Auch </td>
+            <td> 06 15 28 88 63 </td>
+			<td> marcorib1@hotmail.com </td>
+        </tr>
+        <tr>
+            <td> Houssine	Zarrik </td>
+            <td> 32410  </td>
+            <td> Castera-Verduzan </td>
+            <td> 07 85 70 24 93 </td>
+			<td> houssine.zarrik@orange.fr </td>
+        </tr>
+        <tr>
+            <td> Dylan	Gouzenne </td>
+            <td> 32230  </td>
+            <td> Marciac </td>
+            <td> 06 69 70 58 13 </td>
+			<td> dylan.gouzenne@gmail.com </td>
+        </tr>
+        <tr>
+            <td> Benjamin Sandras </td>
+            <td> 32000  </td>
+           	<td> Auch </td>
+            <td> 07 83 86 63 53 </td>
+			<td> bensand77@hotmail.fr  </td>
+        </tr>
+        <tr>
+            <td> Helder	Morais </td>
+            <td> 32000  </td>
+            <td> Auch </td>
+            <td> 06 15 77 07 42 </td>
+			<td> morais.helder@hotmail.fr </td>
+        </tr>
+        <tr>
+            <td> Zakia	Segond </td>
+            <td> 32000  </td>
+            <td> Auch </td>
+            <td> 07 88 75 74 92 </td>
+			<td> segondzakia@live.fr </td>
+        </tr>
+        <tr>
+            <td> Max	Taha </td>
+            <td> ???  </td>
+            <td> Sur liste rouge </td>
+            <td> Sur liste rouge </td>
+			<td> tahamohamedfr@gmail.com </td>
+        </tr>
+        <tr>
+            <td> Nicolas Lasportes </td>
+            <td> 32270  </td>
+            <td> Saint Sauvy </td>
+            <td> 06 95 57 69 37 </td>
+			<td> nicolas.lasportes@gmail.com </td>
+        </tr>
+        <tr>
+            <td> Nathan	Clerc </td>
+            <td> 32600  </td>
+            <td> Beaupuy </td>
+            <td> 06 95 65 08 23 </td>
+			<td> clerc-nathan@hotmail.fr </td>
+        </tr>
+        <tr>
+            <td> Dania	Ahmed </td>
+            <td> 32000  </td>
+            <td> Auch </td>
+            <td> 06 18 86 01 78 </td>
+			<td> 94daniak@live.fr </td>
+        </tr>
+          
+        </tbody>
+      </table>
+            
+
+</body>
+
+
+        <footer class="page-footer">
+          
+            <p>© 2018 Copyright simplon</p>
+          
+        </footer>
+
+
+</html>
